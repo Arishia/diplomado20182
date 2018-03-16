b7c2d22c0d21915bd2c8f12de55d8032801c2e36
Alicia Anahí Fernández Hidalgo <alicia_anahi@yahoo.com.mx>
2018-03-16 08:56:18 -0600

    se cambia el tipo de teclado y se cambia el controlador de la vista

---
+++ WorldWrotter/WorldWrotter/ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WorldWrotter
+//
+//  Created by Alicia Anahí Fernández Hidalgo on 16/03/18.
+//  Copyright © 2018 Alicia Anahí Fernández Hidalgo. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
