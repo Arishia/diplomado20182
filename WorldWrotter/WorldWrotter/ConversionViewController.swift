a4e59f75d9c3946073de61cb207ae51762b6765c
Alicia Anahí Fernández Hidalgo <alicia_anahi@yahoo.com.mx>
2018-03-21 00:20:58 -0600

    Se agrega la conversion de farenheit y celsius

--- WorldWrotter/WorldWrotter/ConversionViewController.swift
+++ WorldWrotter/WorldWrotter/ConversionViewController.swift
@@ -11,8 +11,37 @@ import UIKit
 class ConversionViewController: UIViewController {
     
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil }
+    }
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        celsiusLabel.text = textField.text
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
